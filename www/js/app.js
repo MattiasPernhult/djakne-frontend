bf4fbfc2ae40258db3dd188d84a4844e97b017f5
Rasmus Holm <holmrasmus@Rasmuss-Air.lan>
2016-03-01 20:40:54 +0100

    fix: Fixed Homepage URL

--- www/js/app.js
+++ www/js/app.js
@@ -74,6 +74,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','m
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/home');
 
 });
