c8ce58cecd6d25cb7316ad31aa6047cd606bf6f4
Anna Hahre <a.hahre@hotmail.com>
2016-04-05 11:11:37 +0200

    feat: New branch COWORK

--- www/js/factories.js
+++ www/js/factories.js
@@ -77,14 +77,15 @@ angular.module('factories', ['config'])
     }
 
     $http.get(url)
-      .success(function(result) {
-        console.log('success: ' + result);
-        events = result.data.result;
+      .success(function(res) {
+        console.log('success: ' + res)
+        console.log(JSON.stringify(res));
+        events = res.result;
         return done(events);
       })
       .error(function(err) {
         console.log('ERROR' + err);
-        return done(err.data.error);
+        return done(err.error);
       });
   };
 
