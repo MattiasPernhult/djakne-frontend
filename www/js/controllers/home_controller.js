b0fce8180ff58242ddd754f9a79579ba202066a6
Rasmus Holm <holm.rasmus@hotmail.com>
2016-04-13 12:11:02 +0200

    feat: Added new list cards to home-page

--- www/js/controllers/home_controller.js
+++ www/js/controllers/home_controller.js
@@ -89,13 +89,14 @@ controllers.controller('HomeController', function($scope, CoffeeFactory, $http, 
 
   $scope.next = function() {
     $ionicSlideBoxDelegate.next();
-   };
+  };
+
   $scope.previous = function() {
      $ionicSlideBoxDelegate.previous();
    };
 
-   // Called each time the slide changes
-   $scope.slideChanged = function(index) {
+  // Called each time the slide changes
+  $scope.slideChanged = function(index) {
      $scope.slideIndex = index;
    };
 });
