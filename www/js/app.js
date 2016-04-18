8bd366099df49aea8f24620c9042442a6adb1b1f
Rasmus Holm <holm.rasmus@hotmail.com>
2016-04-18 14:02:45 +0200

    feat: Added new Event navigation

--- www/js/app.js
+++ www/js/app.js
@@ -42,40 +42,10 @@ angular.module('starter', ['ionic', 'ionic-ratings', 'controllers', 'factories',
       controller: 'ProductController',
     })
 
-    .state('eventMain', {
-        url: '/eventMain',
-        templateUrl: 'templates/eventMain.html',
-        controller: 'EventController',
-      })
-
-    .state('events_description', {
-      url: '/events_description',
-      templateUrl: 'templates/events_description.html',
-      controller: 'EventDescriptionController',
-    })
-
     .state('addEvent', {
       url: '/addEvent',
       templateUrl: 'templates/addEvent.html',
       controller: 'AddEventController',
-    })
-
-    .state('boardMain', {
-      url: '/boardMain',
-      templateUrl: 'templates/boardMain.html',
-      controller: 'EventController',
-    })
-
-    .state('newsMain', {
-      url: '/newsMain',
-      templateUrl: 'templates/newsMain.html',
-      controller: 'EventController',
-    })
-
-    .state('memberships', {
-      url: '/memberships',
-      templateUrl: 'templates/memberships.html',
-      controller: 'EventController',
     })
 
     .state('profile', {
@@ -102,16 +72,6 @@ angular.module('starter', ['ionic', 'ionic-ratings', 'controllers', 'factories',
       },
     },
   })
-
-  .state('tab.co', {
-      url: '/co',
-      views: {
-        'tab-co': {
-          templateUrl: 'templates/tab-co.html',
-          controller: 'EventController',
-        },
-      },
-    })
 
   .state('tab.menu', {
     url: '/menu',
